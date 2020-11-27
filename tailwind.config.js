a96dbb3b8686a74738527725bebdff3b03f973c9
İlyas Akın <ashnwor@gmail.com>
2020-11-27 06:58:19 +0300

    misc: Add encode sans condensed font

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			fontFamily: {
+				'encode-sans': ['Encode Sans Condensed', 'sans-serif'],
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
