c82bf9690edc86a4090cbf52aafc4816016b175f
İlyas Akın <ashnwor@gmail.com>
2020-11-28 20:11:01 +0300

    misc: Add bunch of buttons

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1,4 +1,5 @@
 export { default as Github } from './Github'
+export { default as Graphql } from './Graphql'
 export { default as Linkedin } from './Linkedin'
 export { default as Mail } from './Mail'
 export { default as Medium } from './Medium'
