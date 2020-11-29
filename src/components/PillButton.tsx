a1ebdeedf30a8daf8ac504ff1a28e5eca9d93bf8
İlyas Akın <ashnwor@gmail.com>
2020-11-29 15:57:41 +0300

    general: Add project links

--- src/components/PillButton.tsx
+++ src/components/PillButton.tsx
@@ -4,6 +4,7 @@ interface Props {
 	color: string;
 	hoverColor: string;
 	ringColor: string;
+	href: string;
 }
 
 const PillButton: React.FC<Props> = ({
@@ -11,13 +12,17 @@ const PillButton: React.FC<Props> = ({
 	color,
 	hoverColor,
 	ringColor,
+	href,
 }) => {
 	return (
-		<button
-			className={`${color} hover:${hoverColor} h-10 rounded-full px-4 font-encode-sans focus:outline-none focus:ring-2 focus:${ringColor} focus:ring-opacity-50`}
+		<a
+			type="button"
+			href={href}
+			target="__blank"
+			className={`${color} hover:${hoverColor} block rounded-full px-4 py-2 font-encode-sans focus:outline-none focus:ring-2 focus:${ringColor} focus:ring-opacity-50`}
 		>
 			{children}
-		</button>
+		</a>
 	);
 };
 
