1c9385670888469627f6202232928f5a9ebc796c
İlyas Akın <ashnwor@gmail.com>
2021-02-01 21:26:26 +0300

    Add IconLink component

---
+++ src/components/IconLink/IconLink.tsx
@@ -0,0 +1,17 @@
+import './IconLink.scss';
+
+interface Props {
+	Icon: React.FC<{ width: string; height: string }>;
+	width: string;
+	height: string;
+	href: string;
+}
+const IconLink: React.FC<Props> = ({ width, height, Icon, href }) => {
+	return (
+		<a className="icon-link" href={href} target="__blank">
+			<Icon width={width} height={height} />
+		</a>
+	);
+};
+
+export default IconLink;
