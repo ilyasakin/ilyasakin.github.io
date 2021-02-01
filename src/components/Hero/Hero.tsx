cc608acee6d9f8ce8005a36b0147bdcd946facf1
İlyas Akın <ashnwor@gmail.com>
2021-02-01 21:26:26 +0300

    Add IconLink component

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,4 +1,6 @@
 import { Dribbble, Github, LinkedinAlt, Mail, Medium, Resume } from '../icons';
+import IconLink from '../IconLink';
+
 import './Hero.scss';
 
 interface Props {
@@ -12,12 +14,42 @@ const Hero: React.FC<Props> = ({ title, text }) => {
 			<h1 className="hero-title">{title}</h1>
 			<div className="hero-text">{text}</div>
 			<div className="hero-social">
-				<Github height="24px" width="24px" />
-				<Dribbble height="24px" width="24px" />
-				<Mail height="24px" width="32px" />
-				<Medium height="24px" width="42.26px" />
-				<LinkedinAlt height="24px" width="24px" />
-				<Resume height="24px" width="32px" />
+				<IconLink
+					Icon={Github}
+					height="24px"
+					width="24px"
+					href="https://github.com/iakindev"
+				/>
+				<IconLink
+					Icon={Dribbble}
+					height="24px"
+					width="24px"
+					href="https://dribbble.com/ashnwor"
+				/>
+				<IconLink
+					Icon={Mail}
+					height="24px"
+					width="32px"
+					href="mailto:ilyas.akin@yahoo.com"
+				/>
+				<IconLink
+					Icon={Medium}
+					height="24px"
+					width="42.26px"
+					href="https://ilyasakin.medium.com"
+				/>
+				<IconLink
+					Icon={LinkedinAlt}
+					height="24px"
+					width="24px"
+					href="https://www.linkedin.com/in/iakindev"
+				/>
+				<IconLink
+					Icon={Resume}
+					height="24px"
+					width="24px"
+					href="/resume.pdf"
+				/>
 			</div>
 		</div>
 	);
