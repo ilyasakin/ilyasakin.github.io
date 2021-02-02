9f79fd91c13accead515a8c02ad3afef8f353921
İlyas Akın <ashnwor@gmail.com>
2021-02-02 22:17:18 +0300

    Move link url's to a separate file

---
+++ src/Links.ts
@@ -0,0 +1,12 @@
+import LinksType from './types/Links';
+
+const Links: LinksType = {
+  GITHUB: 'https://github.com/iakindev',
+  DRIBBBLE: 'https://dribbble.com/ashnwor',
+  MAIL: 'mailto:ilyas.akin@yahoo.com',
+  MEDIUM: 'https://ilyasakin.medium.com',
+  LINKEDIN: 'https://www.linkedin.com/in/iakindev',
+  RESUME: '/resume.pdf',
+};
+
+export default Links;
