004940ed2da433fa7df55744e5cf5fe7de188032
İlyas Akın <ashnwor@gmail.com>
2021-02-03 23:13:37 +0300

    Add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import Hero from './components/Hero';
 import ProjectsGrid from './components/ProjectsGrid';
+import Footer from './components/Footer';
 
 import links from './Links';
 import projects from './Projects';
@@ -18,6 +19,7 @@ const App = () => {
         />
         <ProjectsGrid projects={projects} />
       </div>
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+import './Footer.scss';
+
+const Footer: React.FC = () => {
+  return <div className="footer">Designed with Figma with ♡</div>;
+};
+
+export default Footer;
