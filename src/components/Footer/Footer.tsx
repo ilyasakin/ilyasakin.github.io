5f07ecf0c39c67486f7b71db18df5d0c55b1cf2b
İlyas Akın <ashnwor@gmail.com>
2021-02-07 23:04:34 +0300

    Replace unicode heart with emoji

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import './Footer.scss';
 
 const Footer: React.FC = () => {
-  return <div className="footer">Designed with Figma with ♡</div>;
+  return <div className="footer">Designed with Figma with ❤️</div>;
 };
 
 export default Footer;
