f73178637e357f8fbb4880760397d56a54842748
İlyas Akın <ashnwor@gmail.com>
2021-02-09 15:56:58 +0300

    Convert more px units to rem

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import './Header.scss';
 const Header: React.FC = () => {
   return (
     <div className="header">
-      <Logo width="32px" height="32px" color="#000000" />
+      <Logo width="2rem" height="2rem" color="#000000" />
       <Pagination />
     </div>
   );
