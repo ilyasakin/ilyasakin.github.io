addefc0eaebf86e00602093e0dc95febca08af90
İlyas Akın <ashnwor@gmail.com>
2021-02-14 16:40:52 +0300

    Add Deliquus and Spotify Clone projects

--- src/Projects.ts
+++ src/Projects.ts
@@ -4,8 +4,15 @@ import SpotifyPNG from './assets/images/spotify-clone.png';
 
 const Projects: Project[] = [
   {
-    title: 'Hello',
-    text: 'Hello',
+    title: 'Deliquus',
+    text: `
+    A cli coverage tool but checks if they exists and returns clean exit or error.
+    
+    Written in TypeScript in NodeJS.
+    
+    User can match multiple sources with multiple targets.
+    For example, you have a components directory as a source.
+    You can make it check if tests and stories exist for components.`,
     images: {
       avif: SpotifyAVIF,
       png: SpotifyPNG,
@@ -14,8 +21,11 @@ const Projects: Project[] = [
     liveUrl: '#',
   },
   {
-    title: 'Hello',
-    text: 'Hello',
+    title: 'Spotify Clone',
+    text: `A shallow Spotify Clone with front-end and back-end.
+    
+    Front-end built with React, React Router, TypeScript and SCSS.
+    Back-end built with Express and Mongoose.`,
     images: {
       avif: SpotifyAVIF,
       png: SpotifyPNG,
