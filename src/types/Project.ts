999315b667e44bede526773a0f2581f606d346e6
İlyas Akın <ashnwor@gmail.com>
2021-03-03 23:30:22 +0300

    Remove 480 breakpoint

--- src/types/Project.ts
+++ src/types/Project.ts
@@ -2,8 +2,8 @@ interface Project {
   TITLE: string;
   DESCRIPTION: string;
   IMAGES: {
-    AVIF: { 480: string; 720: string; 1080: string };
-    JPG: { 480: string; 720: string; 1080: string };
+    AVIF: { 720: string; 1080: string };
+    JPG: { 720: string; 1080: string };
   };
   SOURCE_URL: string;
   LIVE_URL?: string;
