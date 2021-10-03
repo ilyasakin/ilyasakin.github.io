a97a157fed983615f5616555472a4981623505de
Ilyas Akin <ashnwor@gmail.com>
2021-10-03 21:40:36 +0300

    Add articles section

--- src/content/articles.ts
+++ src/content/articles.ts
@@ -25,6 +25,7 @@ const articles: IArticle[] = [
       AVIF: { 720: NodeRedAVIF_720, 1080: NodeRedAVIF_1080 },
       JPG: { 720: NodeRedPNG_720, 1080: NodeRedPNG_1080 },
     },
+    URL: 'https://ilyasakin.medium.com/node-red-is-bad-for-your-enterprise-grade-services-68d7215355f9',
   },
   {
     TITLE: 'You should learn UI/UX as a front-end developer',
@@ -32,6 +33,7 @@ const articles: IArticle[] = [
       AVIF: { 720: UIUXArticleAVIF_720, 1080: UIUXArticleAVIF_1080 },
       JPG: { 720: UIUXArticleJPEG_720, 1080: UIUXArticleJPEG_1080 },
     },
+    URL: 'https://ilyasakin.medium.com/you-should-learn-ui-ux-as-a-front-end-developer-58f8b434690f',
   },
   {
     TITLE: 'You should avoid Tailwind',
@@ -39,6 +41,7 @@ const articles: IArticle[] = [
       AVIF: { 720: AvoidTailwindAVIF_720, 1080: AvoidTailwindAVIF_1080 },
       JPG: { 720: AvoidTailwindJPEG_720, 1080: AvoidTailwindJPEG_1080 },
     },
+    URL: 'https://ilyasakin.medium.com/you-should-avoid-tailwind-3e6fd32a1ded',
   },
 ];
 
