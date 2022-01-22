edf8594aa39fc5d8bce154fa34048cd63260928b
Ilyas Akin <ashnwor@gmail.com>
2022-01-22 20:20:12 +0300

    Remove pagination component for now

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,10 @@
 import { Logo } from '../icons';
-import Pagination from '../pagination';
 import './header.scss';
 
 const Header: React.FC = () => {
   return (
     <div className="header">
       <Logo width="2rem" height="2rem" color="#000000" />
-      <Pagination />
     </div>
   );
 };
