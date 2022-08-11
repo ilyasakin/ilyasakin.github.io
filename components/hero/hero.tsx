8025ac6de4c3f19283ec7b690bc1ccd03ea8edc8
İlyas Akın <ashnwor@gmail.com>
2022-08-11 18:14:15 +0300

    Add click to copy

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -7,8 +7,23 @@ import MailIcon from "../../assets/icons/mail.svg";
 import MediumIcon from "../../assets/icons/medium.svg";
 import LinkedInIcon from "../../assets/icons/linkedin.svg";
 import ResumeIcon from "../../assets/icons/resume.svg";
+import { Tooltip } from "@nextui-org/react";
+import { useState } from "react";
 
 const Hero = () => {
+  const [isCopied, setIsCopied] = useState<boolean>(false);
+
+  const tooltipContent = (
+    <span
+      style={{
+        fontSize: "12px",
+        lineHeight: "1.5",
+      }}
+    >
+      {isCopied ? "Copied!" : "Copy to clipboard"}
+    </span>
+  );
+
   return (
     <div className={style.hero__container}>
       <h1 className={style.hero__title}>
@@ -30,11 +45,20 @@ const Hero = () => {
             <Image src={DribbbleIcon} alt="Dribbble" />
           </a>
         </Link>
-        <Link href="mailto:ilyas.akin@yahoo.com" passHref>
-          <a target="_blank" rel="noreferrer noopener">
-            <Image src={MailIcon} alt="Mail" />
-          </a>
-        </Link>
+        <Tooltip content={tooltipContent}>
+          <Image
+            style={{ cursor: "pointer" }}
+            src={MailIcon}
+            alt="Mail"
+            height={27}
+            width={33}
+            onClick={() => {
+              navigator.clipboard.writeText("ilyas.akin@yahoo.com");
+              setIsCopied(true);
+            }}
+            onMouseLeave={() => setTimeout(() => setIsCopied(false), 150)}
+          />
+        </Tooltip>
         <Link href="https://ilyasakin.medium.com" passHref>
           <a target="_blank" rel="noreferrer noopener">
             <Image src={MediumIcon} alt="Medium" />
