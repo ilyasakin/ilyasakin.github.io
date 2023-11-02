d1cd813c4cb89e456bb5b793212cef2e282f8860
İlyas Akın <ashnwor@gmail.com>
2023-11-02 10:15:02 +0300

    Switch to Jetbrains Mono

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,28 @@
-import { GeistMono } from 'geist/font'
 import { Metadata } from 'next'
 import '../styles/globals.css'
+import { JetBrains_Mono } from 'next/font/google'
+
 export const metadata: Metadata = {
-  title: 'Home',
-  description: 'Welcome to Next.js',
+  title: 'Ilyas Akin',
+  description: 'Personal website of Ilyas Akin',
 }
 
+const jetbrainsMono = JetBrains_Mono({
+  subsets: ['latin', 'latin-ext'],
+  weight: "700",
+  style: "normal"
+});
+
 export default function RootLayout({
-                                     // Layouts must accept a children prop.
-                                     // This will be populated with nested layouts or pages
-                                     children,
-                                   }: {
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={GeistMono.className}>
-    <body>{children}</body>
+    <html lang="en" className={jetbrainsMono.className}>
+      <body>{children}</body>
     </html>
   )
 }
