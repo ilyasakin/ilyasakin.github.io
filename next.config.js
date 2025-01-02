67f9bc3cbd755028e752f7d5afe67df8c7e8eca7
İlyas Akın <ashnwor@gmail.com>
2025-01-02 07:30:40 +0300

    fix: misc fixes, explicit types

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    // Enable static generation for dynamic routes
-    staticPageGenerationTimeout: 120,
-  },
 }
 
 module.exports = nextConfig
