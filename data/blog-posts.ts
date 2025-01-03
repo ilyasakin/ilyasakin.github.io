5e65e7cc6d5d54639e6ff329f391a9bd15388311
İlyas Akın <ashnwor@gmail.com>
2025-01-03 05:08:53 +0300

    feat: remove local blogs until we categorize

--- data/blog-posts.ts
+++ data/blog-posts.ts
@@ -22,22 +22,22 @@ export interface MediumBlogPost {
 export type BlogPost = LocalBlogPost | MediumBlogPost;
 
 export const LOCAL_BLOG_POSTS: LocalBlogPost[] = [
-  {
-    slug: "exile-on-the-main-st",
-    title: "Exile on the Main St",
-    preview: "!Hola¡ from a new day in glorious hell called planet earth of ours...",
-    pubDate: "2025-01-03T02:00:00Z",
-    content: `
-!Hola¡ from a new day in glorious hell called planet earth of ours.
+//   {
+//     slug: "exile-on-the-main-st",
+//     title: "Exile on the Main St",
+//     preview: "!Hola¡ from a new day in glorious hell called planet earth of ours...",
+//     pubDate: "2025-01-03T02:00:00Z",
+//     content: `
+// !Hola¡ from a new day in glorious hell called planet earth of ours.
 
-Today marks a new arc in my life which includes me growing a pair and accepting my shortcomings and dealing with them with a collossal fucking hammer.
+// Today marks a new arc in my life which includes me growing a pair and accepting my shortcomings and dealing with them with a collossal fucking hammer.
 
-Even, a villain arc as some might say.
+// Even, a villain arc as some might say.
 
-Life is too short for your, and, especially your bullshit for your own fucking sake.
+// Life is too short for your, and, especially your bullshit for your own fucking sake.
 
-Time to grow the fuck up.
-`,
-    isLocal: true
-  }
+// Time to grow the fuck up.
+// `,
+//     isLocal: true
+//   }
 ]; 
